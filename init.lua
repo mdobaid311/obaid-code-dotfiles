2aa224f85bdaa4dd531f6f0d09d822c0c7125053
Mohammed Obaid <mdobaid311@gmail.com>
2024-09-08 01:20:12 +0530

    editor config added

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require 'nvim-treesitter.install'.compilers = { "zig" }
-require("treesitter")
+require("nvim-treesitter.install").compilers = { "zig" }
