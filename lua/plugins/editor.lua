56d3ebd76289c92dced000477187ca39ef420488
Mohammed Obaid <mdobaid311@gmail.com>
2024-09-08 02:44:38 +0530

    coding and ui config added

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -53,7 +53,7 @@ return {
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build --config Release; cmake --install build --prefix build",
       },
       "nvim-telescope/telescope-file-browser.nvim",
     },
