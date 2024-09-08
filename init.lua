0e97c7e5ef98f81e7efa55473ea05816aa858f3b
Mohammed Obaid <mdobaid311@gmail.com>
2024-09-08 20:31:23 +0530

    coding and ui config added

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("nvim-treesitter.install").compilers = { "zig" }
+require("nvim-treesitter.install").compilers = { "zig", "cc", "gcc", "clang" }
